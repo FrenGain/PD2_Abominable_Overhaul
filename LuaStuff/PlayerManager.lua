0c08f16e12537b8f2f94cfe3863b52064a5c662d
FrenGain <frengainium@gmail.com>
2024-11-16 12:50:02 -0500

    Adding logs to debug

--- LuaStuff/PlayerManager.lua
+++ LuaStuff/PlayerManager.lua
@@ -2,14 +2,18 @@ Hooks:PostHook(PlayerManager, "damage_reduction_skill_multiplier", "adding_more_
 
     local multiplier = 1
 
+    log("before armo DR is applied")
     local player_unit = self:player_unit()
     if alive(player_unit) then
+    log("are you alive? then")
         local dmg_ext = player_unit:character_damage()
         local has_armor = dmg_ext:get_real_armor() > 0
+    log("do you have armor? if you do")
         if has_armor then
             multiplier = multiplier * self:upgrade_value("player", "armor_damage_reduction_1", 1)
             multiplier = multiplier * self:upgrade_value("player", "armor_damage_reduction_2", 1)
             multiplier = multiplier * self:upgrade_value("player", "armor_damage_reduction_3", 1)
+    log("apply the DR")
     end
 end
 
