722c4907173b24c6e4975ce130f91dae8e0ebb4a
FrenGain <frengainium@gmail.com>
2024-11-18 16:48:32 -0500

    Syntax crash

--- LuaStuff/SkillTreeTweakData.lua
+++ LuaStuff/SkillTreeTweakData.lua
@@ -14,26 +14,25 @@ Hooks:PostHook(SkillTreeTweakData, "init", "rebalance_skill_additions", function
     --Resilience/Shock & Awe/Iron Man DR additions/replacements
 
     self.skills.oppressor[2].upgrades[1] = "player_armor_damage_reduction_1" --SHOULD replace Resilience Aced with armor DR. Will have to test.
-    table.insert(self.skills.iron_man[1].upgrades, "player_armor_damage_reduction_2") --Adding armor DR to Shock and Awe Basic, renamed "Overwhelm".
-    table.insert(self.skills.juggernaut[2].upgrades, "player_armor_damage_reduction_3") --Giving Iron Man Aced armor DR. Skill will be renamed to Juggernaught.
+    table.insert(self.skills.iron_man[1].upgrades[1], "player_armor_damage_reduction_2") --Adding armor DR to Shock and Awe Basic, renamed "Overwhelm".
+    table.insert(self.skills.juggernaut[2].upgrades[1], "player_armor_damage_reduction_3") --Giving Iron Man Aced armor DR. Skill will be renamed to Juggernaught.
 
 
     --Headshot temp DR effect tied to Bullseye
     
-    table.insert(self.skills.prison_wife[1].upgrades, "player_headshot_damage_reduction_bonus_1") --For giving the temp DR on headshots
-    table.insert(self.skills.prison_wife[2].upgrades, "player_headshot_damage_reduction_bonus_2")
+    table.insert(self.skills.prison_wife[1].upgrades[1], "player_headshot_damage_reduction_bonus_1") --For giving the temp DR on headshots
+    table.insert(self.skills.prison_wife[2].upgrades[1], "player_headshot_damage_reduction_bonus_2")
 
 
     --Heavy Impact damage stuff
 
-    table.insert(self.skills.heavy_impact[1].upgrades, "weapon_heavy_impact_damage_multiplier_1") --Extra damage for Heavy Impact tiers - Makes it more impactful. HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    table.insert(self.skills.heavy_impact[2].upgrades, "weapon_heavy_impact_damage_multiplier_2")
+    table.insert(self.skills.heavy_impact[1].upgrades[1], "weapon_heavy_impact_damage_multiplier_1") --Extra damage for Heavy Impact tiers - Makes it more impactful. HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    table.insert(self.skills.heavy_impact[2].upgrades[1], "weapon_heavy_impact_damage_multiplier_2")
 
 
     --Lock n' Load/Fire Control(changing that shit to Firing Line) stuff
 
-    table.insert(self.skills.fire_control{1}.upgrades, "player_run_and_shoot_1") --Giving Fire Control(Firing Line) hip-fire-while-sprinting capabilities
-
-
+    table.insert(self.skills.fire_control[1].upgrades[1], "player_run_and_shoot_1") --Giving Fire Control(Firing Line) hip-fire-while-sprinting capabilities
+    self.skills.shock_and_awe[1].upgrades[1] = "weapon_passive_reload_speed_multiplier_custom_1" --Replacing Lock n' Load Basic with a 25% reload speed bump
 
 end) 
